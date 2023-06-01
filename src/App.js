226034fe71319bcc46f9dece0217819181ba0ba8
Chatterjee, Tapas <tchatterjee@rednucleus.com>
2023-06-01 16:05:04 +0530

    refactored some code

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import Carousal from "./components/Carousal/Carousal";
 import { useState } from "react";
 import { useReducer } from "react";
 import { reducerFunction } from "./reducers/index";
+import { imageListData } from "./data/imageList";
 
 function App() {
-  const [imageList] = useState([
-    "https://static.sadhguru.org/d/46272/1633199491-1633199490440.jpg",
-    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-    "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072821_1280.jpg",
-    "https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567_1280.jpg",
-  ]);
-
-  const [App_Reducer_State, dispatch] = useReducer(reducerFunction, {
+  const [imageList] = useState(imageListData);
+  const initialState = {
     ImageIndex: 0,
     maxLength: imageList.length,
-  });
+  };
+  const [App_Reducer_State, dispatch] = useReducer(
+    reducerFunction,
+    initialState
+  );
 
   const onNext = () => {
     dispatch({ type: "Go_Next" });
---
+++ src/data/imageList.js
@@ -0,0 +1,6 @@
+export const imageListData = [
+  "https://static.sadhguru.org/d/46272/1633199491-1633199490440.jpg",
+  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+  "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072821_1280.jpg",
+  "https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567_1280.jpg",
+];
